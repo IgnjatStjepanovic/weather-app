8a28830861ef9eac22bb11dfd8d9aa27c46c476d
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-08-18 22:21:43 +0200

    Corrected the promise chain. Put in Berlin as default city.

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-let city = document.getElementById('city');
-let temp = document.getElementById('temp');
-let wind = document.getElementById('wind');
-let humidity = document.getElementById('humidity');
-let pressure = document.getElementById('pressure');
+let city = document.getElementById("city");
+let temp = document.getElementById("temp");
+let wind = document.getElementById("wind");
+let humidity = document.getElementById("humidity");
+let pressure = document.getElementById("pressure");
 
+function search() {
+  citySearch = document.getElementById("citysearch").value;
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      citySearch +
+      "&APPID=64d27e418c94ae459b8862f6019c05eb"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      city.innerHTML = "Location: " + data.name + ", " + data.sys.country;
+      temp.innerHTML =
+        "Temperature: " + (parseInt(data.main.temp) - 273) + "&#8451";
+      wind.innerHTML = "Wind: " + data.wind.speed + " m/s";
+      humidity.innerHTML = "Humidity: " + data.main.humidity + " %";
+      pressure.innerHTML = "Humidity: " + data.main.pressure + " mbar";
+      citySearch = document.getElementById("citysearch").value = "";      
+    });
+}
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=64d27e418c94ae459b8862f6019c05eb')
-  .then(res => res.json())
-  .then(data => obj = data)
-  .then(data => console.log(data))
-  .then(() => city.innerHTML = "Location: " + obj.name + ", " + obj.sys.country)
-  .then(() => temp.innerHTML = "Temperature: " + (parseInt(obj.main.temp) - 273) + "&#8451")
-  .then(() => wind.innerHTML = "Wind: " + obj.wind.speed + " m/s")
-  .then(() => humidity.innerHTML = "Humidity: " + obj.main.humidity + " %")
-  .then(() => pressure.innerHTML = "Humidity: " + obj.main.pressure + " mbar")
-
-
+search();
