bd76e7348c38649726248d7094a335b24a0ae9fd
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-08-19 20:23:01 +0200

    Implemented the weatherlogo and minor changes to styling.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let temp = document.getElementById("temp");
 let wind = document.getElementById("wind");
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
+let logo = document.getElementById("logo");
 
 function search() {
   citySearch = document.getElementById("citysearch").value;
@@ -14,14 +15,16 @@ function search() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      logo.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
       city.innerHTML = "Location: " + data.name + ", " + data.sys.country;
       temp.innerHTML =
         "Temperature: " + (parseInt(data.main.temp) - 273) + "&#8451";
       wind.innerHTML = "Wind: " + data.wind.speed + " m/s";
       humidity.innerHTML = "Humidity: " + data.main.humidity + " %";
-      pressure.innerHTML = "Humidity: " + data.main.pressure + " mbar";
+      pressure.innerHTML = "Pressure: " + data.main.pressure + " mbar";
       citySearch = document.getElementById("citysearch").value = "";      
     });
 }
 
 search();
+
